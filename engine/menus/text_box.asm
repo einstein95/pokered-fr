ff9f9af1453f4ce175ced7ac707361f8d8305c56
Rangi <35663410+Rangi42@users.noreply.github.com>
2025-06-30 12:47:22 -0400

    Replace hardware_constants.asm with hardware.inc (#511)

--- engine/menus/text_box.asm
+++ engine/menus/text_box.asm
@@ -160,7 +160,7 @@ DoBuySellQuitMenu:
 	ld a, BUY_SELL_QUIT_MENU_TEMPLATE
 	ld [wTextBoxID], a
 	call DisplayTextBoxID
-	ld a, A_BUTTON | B_BUTTON
+	ld a, PAD_A | PAD_B
 	ld [wMenuWatchedKeys], a
 	ld a, $2
 	ld [wMaxMenuItem], a
@@ -177,9 +177,9 @@ DoBuySellQuitMenu:
 	ld [wStatusFlags5], a
 	call HandleMenuInput
 	call PlaceUnfilledArrowMenuCursor
-	bit BIT_A_BUTTON, a
+	bit B_PAD_A, a
 	jr nz, .pressedA
-	bit BIT_B_BUTTON, a ; always true since only A/B are watched
+	bit B_PAD_B, a ; always true since only A/B are watched
 	jr z, .pressedA
 	ld a, CANCELLED_MENU
 	ld [wMenuExitMethod], a
@@ -217,7 +217,7 @@ DisplayTwoOptionMenu:
 	ld [wChosenMenuItem], a
 	ld [wMenuExitMethod], a
 
-	ld a, A_BUTTON | B_BUTTON
+	ld a, PAD_A | PAD_B
 	ld [wMenuWatchedKeys], a
 	ld a, $1
 	ld [wMaxMenuItem], a
@@ -299,7 +299,7 @@ DisplayTwoOptionMenu:
 	pop hl
 .noYesMenuInputLoop
 	call HandleMenuInput
-	bit BIT_B_BUTTON, a
+	bit B_PAD_B, a
 	jr nz, .noYesMenuInputLoop ; try again if B was not pressed
 	pop af
 	pop hl
@@ -312,7 +312,7 @@ DisplayTwoOptionMenu:
 	ld [wTwoOptionMenuID], a
 	call HandleMenuInput
 	pop hl
-	bit BIT_B_BUTTON, a
+	bit B_PAD_B, a
 	jr nz, .choseSecondMenuItem ; automatically choose the second option if B is pressed
 .pressedAButton
 	ld a, [wCurrentMenuItem]
